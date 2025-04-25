2e42cabaec3cb296bfadd9a801cd10c94c8e996a
nikita <nikitabrworkjob@gmail.com>
2025-04-25 22:39:45 +0300

    filtr

--- store/models.py
+++ store/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    from django.shortcuts import render
+from .models import Product, Category
+
+def home(request):
+    category_id = request.GET.get('category')
+    categories = Category.objects.all()
+
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
+    return render(request, 'store/home.html', {
+        'products': products,
+        'categories': categories,
+        'selected_category': int(category_id) if category_id else None
+    })
