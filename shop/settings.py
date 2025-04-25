0f5c1532d2b598e62016dd73ad5b29b283ecb669
nikita <nikitabrworkjob@gmail.com>
2025-04-25 20:31:54 +0300

    css file + home.html fix

--- shop/settings.py
+++ shop/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # указываем на директорию, где находятся статические файлы
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
