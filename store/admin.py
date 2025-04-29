0f2117bb7b59ce7e5d7d801624257eab18f57f27
nikita <nikitabrworkjob@gmail.com>
2025-04-29 18:10:17 +0300

    Global fix product_html

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Characteristic, ProductImage
 
-admin.site.register(Product)
+
+
+class CharacteristicInline(admin.TabularInline):
+    model = Characteristic
+    extra = 1
+
+class ProductImageInline(admin.TabularInline):  # Можно заменить на StackedInline — будет вертикально
+    model = ProductImage
+    extra = 1  
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+    inlines = [CharacteristicInline, ProductImageInline]
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
-
--- store/models.py
+++ store/models.py
@@ -17,8 +17,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
-    
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')  # ВАЖНО
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f"{self.product.title} - image"
+
 class Order(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='orders')
     name = models.CharField(max_length=100)
@@ -28,3 +34,11 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Заказ от {self.name} ({self.phone}) на {self.product.title}"
+
+class Characteristic(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='characteristics')
+    name = models.CharField(max_length=100)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}: {self.value}"
